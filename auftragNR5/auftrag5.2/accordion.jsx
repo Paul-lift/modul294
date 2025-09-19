2df95198cca64744dd83dec225378ba36fd980be
Paul-lift <paulbot56@gmail.com>
2025-09-19 11:43:41 +0200

    Update accordion icons for improved visual representation

--- auftragNR5/auftrag5.2/accordion.jsx
+++ auftragNR5/auftrag5.2/accordion.jsx
@@ -16,19 +16,19 @@ function Accordion() {
       title: "Section 2",
       content:
         "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      icon: "🤪",
+      icon: "🦁",
     },
     {
       title: "Section 3",
       content:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-      icon: "🤪",
+      icon: "🌹",
     },
     {
       title: "Section 4",
       content:
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      icon: "🤪",
+      icon: "🤓",
     },
   ];
 
